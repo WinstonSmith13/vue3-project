cd3366729e25845c09d6e41540e99e3b57046c76
Medy <krouk.medy@gmail.com>
2022-11-01 16:22:31 +0100

    Working on the nav bar.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
-<RouterView />
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <site-navigation></site-navigation>
+    <RouterView/>
   </div>
 </template>
 
 <script setup>
-import{RouterView} from "vue-router";
+
+
+import SiteNavigation from "./components/SiteNavigation.vue";
+
+
+
 </script>
 
 <style scoped>
